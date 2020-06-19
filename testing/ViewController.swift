31968e5618b55d1e40b5e72051dfd5a8480ee4f5
Maggie Guan <maggieguan149@gmail.com>
2020-06-18 21:47:38 -0700

    Initial Commit

---
+++ testing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testing
+//
+//  Created by Maggie Guan on 6/18/20.
+//  Copyright © 2020 Maggie Guan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
